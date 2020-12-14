896fc4d6086c8b27dad200b84d10d1a3ecee58db
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-12-14 00:13:49 -0600

    returned colors and passed to single color

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ function App() {
   const [list, setList] = useState([]);
 
   const handleSubmit = (e) => {
-    e.preventDefualt();
-    console.log("Handle Submit");
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
@@ -24,6 +30,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
+            className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
             Submit
@@ -31,7 +38,10 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>List goes here</h4>
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </>
   );
