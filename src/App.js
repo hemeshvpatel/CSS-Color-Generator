970cc7ae71aca052c3d78dd67a0a10b1444a09e0
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-12-15 15:06:37 -0600

    copy to clipboard complete with 3 second alert and default value set up on load

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#008000").all(8));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      let colors = new Values(color).all(10);
+      let colors = new Values(color).all(8);
       setList(colors);
     } catch (error) {
       setError(true);
@@ -23,13 +22,13 @@ function App() {
   return (
     <>
       <section className="container">
-        <h3>CSS Color Picker</h3>
+        <h3>CSS Color Picker (click color to copy to cliboard)</h3>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder="#f15025"
+            placeholder="#008000"
             className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -4,16 +4,28 @@ import React, { useState, useEffect } from "react";
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const background = rgb.join(",");
-
   // const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
 
   return (
     <article
       className={`color`}
       style={{ backgroundColor: `rgb(${background})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hexColor}</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">Copied to Clipboard</p>}
     </article>
   );
 };
